cf7ed7f3a8c7ea854d513527a442519d90e075d6
Ahmed Negm <ahmed.negm@mendix.com>
2022-09-29 14:38:44 +0200

    Replace verse links

--- src/script.ts
+++ src/script.ts
@@ -3,14 +3,20 @@ import * as fs from "fs-extra";
 import * as glob from "glob";
 
 const zcPath = process.env.zcPath!;
-interface synonymous {
+interface ISynonymous {
     quote: string;
     word: string;
     definition: string;
 }
 
+interface IVerse {
+    link: string;
+    ayah: string;
+}
+
 async function main() {
-    const synonyms: synonymous[] = [];
+    const synonyms: ISynonymous[] = [];
+    const allverses = getAllverses();
     const allFiles = await getMarkdownFiles(zcPath);
     for (const file of allFiles) {
         const content = await fs.readFile(path.join(zcPath, file), "utf-8");
@@ -23,10 +29,14 @@ async function main() {
                         for (const synonymsLine of synonymsLines) {
                             const wordEndIndex = synonymsLine.indexOf("**", 4);
                             const word = synonymsLine.substring(4, wordEndIndex).trim();
-                            const definition = synonymsLine
-                                .substring(wordEndIndex + 2)
-                                .replace(/^\:+|\:+$/g, "")
-                                .trim();
+                            const definition = replaceVerseLinks(
+                                synonymsLine
+                                    .substring(wordEndIndex + 2)
+                                    .replace(/^\:+|\:+$/g, "")
+                                    .trim(),
+                                allverses
+                            );
+
                             synonyms.push({ quote: lines[0].trim(), word, definition });
                         }
                     }
@@ -38,7 +48,7 @@ async function main() {
     fs.writeFileSync(
         "test.txt",
         synonyms
-            .map(synonymous => `quote: "${synonymous.quote}"\nword: "${synonymous.word}"\ndefinition: "${synonymous.definition}"\n`)
+            .map(synonymous => `quote: ${synonymous.quote}\nword: ${synonymous.word}\ndefinition: ${synonymous.definition}\n`)
             .join("\n---\n")
     );
 
@@ -55,4 +65,27 @@ async function getMarkdownFiles(dir: string): Promise<string[]> {
     return new Promise<string[]>((resolve, reject) =>
         glob("**/*.md", globOptions, (err: any, matches: any) => (err ? reject(err) : resolve(matches)))
     );
+}
+
+function getAllverses() {
+    const allverses: IVerse[] = [];
+
+    const quranJsonPath = require.resolve("quran-json/dist/quran.json");
+    const surahs = fs.readJsonSync(quranJsonPath);
+
+    for (const surah of surahs) {
+        for (const verse of surah.verses) {
+            allverses.push({ link: `[[${surah.name}-${verse.id}]]`, ayah: verse.text });
+        }
+    }
+
+    return allverses;
+}
+
+function replaceVerseLinks(definition: string, allverses: IVerse[]) {
+    for (const verse of allverses) {
+        definition = definition.replace(verse.link, `"${verse.ayah}"`);
+    }
+
+    return definition;
 }
